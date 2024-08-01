5273c2d3f19a3cf9b69d8e1433a7fbda6bbfd5c6
João Vitor Oliveira Santos <48342168883@info.local>
2024-08-01 10:33:04 -0300

    push test

---
+++ Calculos.Teste/CalculoUnitTest.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Calculos.Teste
+{
+    public class CalculoUnitTest
+    {
+        // AAA: Arrange, Act, Assert
+
+        [Fact]
+        public void TestarMetodoSomar()
+        {
+            // Arrange
+            var x1 = 4.1;
+            var x2 = 5.9;
+            var valorEsperado = 10;
+
+            // Act
+            var soma = Calculo.somar(x1, x2);
+
+            // Assert
+            Assert.Equal(valorEsperado, soma);
+        }
+
+        [Fact]
+        public void TestarMetodoSubtrair()
+        {
+            // Arrange
+            var x1 = 10;
+            var x2 = 5;
+            var valorEsperado = 5;
+
+            // Act
+            var diferenca = Calculo.subtrair(x1, x2);
+
+            // Assert
+            Assert.Equal(valorEsperado, diferenca);
+        }
+
+        [Fact]
+        public void TestarMetodoMultiplicar()
+        {
+            // Arrange
+            var x1 = 4;
+            var x2 = 5;
+            var valorEsperado = 20;
+
+            // Act
+            var produto = Calculo.multiplicar(x1, x2);
+
+            // Assert
+            Assert.Equal(valorEsperado, produto);
+        }
+
+        [Fact]
+        public void TestarMetodoDividir()
+        {
+            // Arrange
+            var x1 = 20;
+            var x2 = 4;
+            var valorEsperado = 5;
+
+            // Act
+            var quociente = Calculo.dividir(x1, x2);
+
+            // Assert
+            Assert.Equal(valorEsperado, quociente);
+        }
+
+        [Fact]
+        public void TestarMetodoDividirPorZero()
+        {
+            // Arrange
+            var x1 = 20;
+            var x2 = 0;
+
+            // Act & Assert
+            Assert.Throws<DivideByZeroException>(() => Calculo.dividir(x1, x2));
+        }
+    }
+}
---
+++ Calculos/Calculo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculos
+{
+    public static class Calculo
+    {
+        public static double somar(double x, double y)
+        {
+            return x + y;
+        }
+
+        public static double subtrair(double x, double y)
+        {
+            return x - y;
+        }
+
+        public static double multiplicar(double x, double y)
+        {
+            return x * y;
+        }
+
+        public static double dividir(double x, double y)
+        {
+            if (y == 0)
+            {
+                throw new DivideByZeroException("O divisor não pode ser zero.");
+            }
+            return x / y;
+        }
+    }
+}
